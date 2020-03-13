de23c6396f51e81cc1f6e62955414f37eeb25e8e
shallowerer <1758862241@qq.com>
2020-03-13 19:06:06 +0800

    前端vue界面

---
+++ README.md
@@ -0,0 +1,39 @@
+# springboot-vue
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+# 钟尚峻的毕业设计
+
+#### 这是一个简单的前后端分离项目，主要采用 Vue.js + SpringBoot 技术栈开发。
+
+# 技术栈
+
+## 1.前端技术栈
+
+1.Vue  
+2.ElementUI  
+3.axios   
+
+## 2.后端技术栈
+
+1.Spring Boot  
+2.Spring Data + JPA 
+3.MySQL  
+4.Shiro
---
+++ src/views/User.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <td>用户编号</td>
+        <td>用户账号</td>
+        <td>用户名称</td>
+      </tr>
+			<tr v-for="item in users">
+				<td>{{ item.sid }}</td>
+				<td>{{ item.uid }}</td>
+				<td>{{ item.uname }}</td>
+			</tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+	name: "User",
+	data() {
+		return {
+			msg:"hello springboot",
+			users:[
+				{
+					sid: 1,
+					uid: 1,
+					uname: "测试人"
+				},
+				{
+					sid: 1,
+					uid: 1,
+					uname: "测试人"
+				},
+				{
+					sid: 1,
+					uid: 1,
+					uname: "测试人"
+				}
+			]
+		}
+	},
+	created() {
+		axios.get('http://localhost:8899/user/findAll/').then((res)=> { // 当不是箭头函数，此时的this为回调
+			console.log(res)
+			this.users = res.data
+		})
+	
+	}
+
+}
+</script>
+
+<style>
+
+</style>
